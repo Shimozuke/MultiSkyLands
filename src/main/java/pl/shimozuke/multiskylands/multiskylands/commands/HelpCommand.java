8eccb6de4e6b0acfa6960857e664ce02f8d9d655
Jakub <Xanox12@Gmail.com>
2020-07-08 00:00:20 +0200

    reset repozitory

---
+++ src/main/java/pl/shimozuke/multiskylands/multiskylands/commands/HelpCommand.java
@@ -0,0 +1,26 @@
+package pl.shimozuke.multiskylands.multiskylands.commands;
+
+import com.google.common.collect.Lists;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import pl.shimozuke.multiskylands.multiskylands.MultiskyLands;
+
+import javax.xml.soap.Text;
+import java.util.List;
+import java.util.Map;
+
+public class HelpCommand implements CommandExecutor
+{
+
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException
+    {
+        Map<List<String>, CommandSpec> commands = MultiskyLands.SUBCOMMANDS;
+        List<Text> helpList = Lists.newArrayList();
+
+    }
+}
